27682cf2a8a7ae2537a626f255b11538eb646792
mohamed sobhy <msobhy.abdalla@gmail.com>
2019-09-23 09:36:18 +0200

    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-
-  public final String greet( final String someone) {
+/*
+@pram
+@return
+*/
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
